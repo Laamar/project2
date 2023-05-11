a2caa4e07a5990adcb0cce61560c119a9fbadce0
Laamar Adams <78374315+Laamar@users.noreply.github.com>
2023-05-11 06:07:27 -0400

    Second Commit

--- src/main/java/edu/guilford/Driver.java
+++ src/main/java/edu/guilford/Driver.java
@@ -1,35 +1,61 @@
 package edu.guilford;
 
-/**
- * Hello world!
- *
- */
-public class Driver 
-{
-    public static void main( String[] args )
-    {
-        //Test the Constructor and toString method method
-        Rational r = new Rational();
-        System.out.println(r);
 
-        //Test the second Constructor and toString method
-        Rational r2 = new Rational(3, 4);
-        System.out.println(r2);
-        //Test the negate method
-        r2.negate();
-        System.out.println(r2);
-        //Test the invert method
-        r2.invert();
-        System.out.println(r2);
-        //Test the toDouble method
-        System.out.println(r2.toDouble());
-        //Test the reduce method
-       // r2.reduce();
-        //System.out.println(r2);
-        //Test the add method
-        r2.add(r2);
-        System.out.println(r2);
+public class Driver {
+    public static void main( String[] args ){
+        //tests the constructor and toString method.
+        //this test the default constructor.
+        System.out.println("Testing the default constructor.");
+        Rational r1 = new Rational();
+        System.out.println("The rational number is "+ r1 +"\n");
+
+        //tests the getters and setters
+        System.out.println("Testing the getters and setters."+ "\n");
+        Rational r2 = new Rational();
+        //print out the values of r2
+        System.out.println("The rational number is "+ r2);
+        //print out the values of the getters
+        System.out.println("The numerator is:"+ r2.getNumerator());
+        System.out.println("The denominator is:"+ r2.getDenominator()+"\n");
+        //print out the values of the setters
+        System.out.println("Testing the second constructor.");
+        Rational r3 = new Rational();
+        System.out.println("The rational number is "+ r3);
+        //print out the negate method.
+        System.out.println("Testing the negate method.");
+        r3.negate();
+        System.out.println("The negate rational number of is "+ r3);
+        //print out the invert method. 
+        System.out.println("Testing the invert method.");
+        r3.invert();
+        System.out.println("The invert rational number is "+ r3 +"\n");
+        //Write an instance method called toDouble that converts the rational number to a double (floating-point number) and returns the result.
+        System.out.println("Testing the toDouble method."+ "\n");
+        System.out.println("The double of the rational number is "+ r3.toDouble()+"\n");
+         // use the constructor to create a new rational number
+        Rational r4 = new Rational(1, 2);
+        //use the method add to this class to add r3 to r2
+        System.out.println("Here goes a new random generated rational number. "+ r4+"\n");
+        r3.add(r4);
+        //print out the inputs
+        System.out.println("The inputs "+ r3 +" and "+ r4);
+        //print out the result
+        System.out.println("The sum of the rationals numbers, "+ r3 +" and "+ r4 +" is "+ r3.add(r4));
+        //Add code that prints out a warning if the constructor with parameter attempts to generate a Rational number with a 0 denominator.
+        if (r3.add(r4).getDenominator() == 0)
+        {
+            System.out.println("Warning: The denominator is 0."+ "\n");
+        }
+        else
+        {
+            System.out.println("The denominator is not 0."+ "\n");
+        }
 
 
+    
+    
     }
+
+
 }
+    
--- src/main/java/edu/guilford/Rational.java
+++ src/main/java/edu/guilford/Rational.java
@@ -3,59 +3,65 @@ package edu.guilford;
 import java.util.Random;
 
 public class Rational {
-    //Two integers attributes for numerator and denominator
-    private int numerator;
-    private int denominator;
+    //declare two variables to hold the numerator and denominator
+      private int numerator;
+      private int denominator;
+      //constructor
+      public Rational(int num, int den) {
+          numerator = num;
+          denominator = den;
+      }
+      //Add a constructor that takes no arguments and sets the numerator to 0 and denominator to 1.
+      public Rational() {
+          Random rand = new Random();
+          numerator = rand.nextInt(-100,100) ;
+          denominator = rand.nextInt(-100,100);
+      }
+      //getters
+      public int getNumerator() {
+          return numerator;
+      }
+  
+      public int getDenominator() {
+          return denominator;
+      }
 
-    private int gcd(int numerator2, int denominator2) {
-        return 0;
-    }
-
-
-    //Constructor that sets the numerator to 0 and denominator to 1
-    public Rational() {
-        Random rand = new Random();
-        numerator = rand.nextInt(-100,100) ;
-        denominator = rand.nextInt(-100,100);
-    }
-    //Add a second Construcctor that takes two aruments and uses them to initialize the attributs
-    public Rational(int numerator, int denominator) {
-        this.numerator = numerator;
-        this.denominator = denominator;
-    }
-
-
-    //Add a toString method that displays a Rational object in a reasonable format
-    public String toString() {
-        return numerator + "/" + denominator;
-    }
-    //Add a method called negate that reverses the sign of ratuonal number
-    public void negate() {
-        numerator = -numerator;
-    }
-    //Write a method called invert that inverts the number by swapping the numerator and denominator
-    public void invert() {
-        int temp = numerator;
-        numerator = denominator;
-        denominator = temp;
-    }
-    //Write a method called toDouble that converts the rational number to a double and returns the result
-    public double toDouble() {
-        return (double) numerator / denominator;
-    }
-    //Write a method called reduce that reduces the rational number to lowest terms, find the greatest common divisor
-    public void reduce() {
-        int gcd = gcd(numerator, denominator);
-        numerator = numerator / gcd;
-        denominator = denominator / gcd;
-    }
-    //Write a method called add that takes a rational number as an argument and adds it to the current rational number
-    public void add(Rational r) {
-        numerator = numerator * r.denominator + r.numerator * denominator;
-        denominator = denominator * r.denominator;
-        reduce();
-    }
-
-
-
-}
+      //setters
+      public void setNumerator(int num){
+          numerator = num;
+      }
+  
+      public void setDenominator(int den) {
+          denominator = den;
+      }
+      //Add a toString method that displays a Rational object in a reasonable format.
+      public String toString() {
+          return numerator + "/" + denominator;
+      }
+      //Write a method called negate that reverses the sign of the rational number.
+      //we dont need tto return anything because its void.
+  
+      //the void method is used to change the value of the numerator.
+      public void negate(){
+          numerator = -numerator;
+      }
+      //create a method called invert that swaps the numerator and denominator.
+      public void invert() {
+          int temp = numerator;
+          numerator = denominator;
+          denominator = temp;
+      }
+  //Write a method called toDouble that converts the rational number to a double (floating-point number).
+      public double toDouble(){
+          return (double)numerator/denominator;
+      }
+  
+      //Write a method called add that takes a Rational number as an argument, adds it to the rational number represented by the current object, and returns a new Rational object
+      public Rational add(Rational r) {
+          int num = numerator * r.denominator + r.numerator * denominator;
+          int den = denominator * r.denominator;
+          return new Rational(num, den);
+      }
+  
+      
+  }
